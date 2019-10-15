080e4ebe3760ff25e0c712932a61902eb900e53c
HYEIN JU <hyein.ju92@gmail.com>
2019-10-15 22:41:03 +0900

    Fix bug

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 //모든 이모티콘 가져오기
-export const getAllEmoticons = () => axios.get('/emoticons');
+export const getAllEmoticons = () => axios.get(`${process.env.REACT_APP_SERVER}/emoticons`);
 
 //새로운 이모티콘 등록
-export const postNewEmoticon = data => axios.post('/emoticons', data);
+export const postNewEmoticon = data => axios.post(`${process.env.REACT_APP_SERVER}/emoticons`, data);
 
 //태그별로 이모티콘 가져오기
-export const getEmoticonsByTag = tagName => axios.get(`tags?tag=${tagName}`);
+export const getEmoticonsByTag = tagName => axios.get(`${process.env.REACT_APP_SERVER}/tags?tag=${tagName}`);
 
 //카피 누를때마다 해당 이모티콘의 copyCount올리기
 export const increaseCopyCnt = emoticonId =>
-  axios.post(`emoticons/${emoticonId}`);
+  axios.post(`${process.env.REACT_APP_SERVER}/emoticons/${emoticonId}`);
