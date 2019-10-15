565c38a21e11a6266d99caec4944bf2208d14187
HYEIN JU <hyein.ju92@gmail.com>
2019-10-15 16:59:12 +0900

    Add lib code

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,10 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 //모든 이모티콘 가져오기
-export const getAllEmoticons = axios.get('/emoticons');
+export const getAllEmoticons = () => axios.get('/emoticons');
+
+//새로운 이모티콘 등록
+export const postNewEmoticon = data => axios.post('/emoticons', data);
 
 //태그별로 이모티콘 가져오기
-export const getEmoticonsByTag = (tagName) => axios.get(`tags?tag=${tagName}`);
+export const getEmoticonsByTag = tagName => axios.get(`tags?tag=${tagName}`);
 
 //카피 누를때마다 해당 이모티콘의 copyCount올리기
-export const increaseCopyCnt = (emoticonId) => axios.post(`emoticons${emoticonId}`);
+export const increaseCopyCnt = emoticonId =>
+  axios.post(`emoticons/${emoticonId}`);
--- frontend/src/lib/colorPick.js
+++ frontend/src/lib/colorPick.js
@@ -1,16 +1,17 @@
-const color = [
-    '#ffc9c9',
-    '#fcc2d7',
-    '#eebefa',
-    '#d0bfff',
-    '#99e9f2',
-    '#96f2d7',
-    '#ffec99',
-    '#ffd8a8'
-]
+const colors = [
+  '#ffc9c9',
+  '#fcc2d7',
+  '#eebefa',
+  '#d0bfff',
+  '#99e9f2',
+  '#96f2d7',
+  '#ffec99',
+  '#ffd8a8'
+];
 const colorPick = () => {
-    const i = Math.floor(Math.random() * color.length);
-    return color[i];
-}
+  const i = Math.floor(Math.random() * colors.length);
+  return colors[i];
+};
 
+export { colors };
 export default colorPick;
