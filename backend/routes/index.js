1cfa75c3af1ef53ba342e69665cb736b5b3aea26
HyeIn Ju <hyein.ju92@gmail.com>
2020-05-08 21:59:01 +0900

    Chore:디렉토리정리

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { Emoticon, Tag } = require('../models');
-
-router.get('/', (req, res, next) => {
-  return res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
